574611e70fb95e1043c19707780d002a1236bce7
TeRacksito <acho.chuchi.8@gmail.com>
2025-01-12 18:31:18 +0000

    Enhance product details and pricing display with additional information and improved formatting

--- frontend/src/components/Product/ProductDetails.tsx
+++ frontend/src/components/Product/ProductDetails.tsx
@@ -1,4 +1,6 @@
 import { ProductWithFlags } from "@pcc/shared";
+import { FaShop } from "react-icons/fa6";
+import PcLogo from "/pc-logo.svg";
 
 interface ProductDetailsProps {
   productWithFlags: ProductWithFlags;
@@ -10,8 +12,23 @@ export const ProductDetails: React.FC<ProductDetailsProps> = ({
   return (
     <div className="mt-4">
       <h2 className="text-2xl font-semibold">{productWithFlags.name}</h2>
-      <p className="mt-2 text-gray-600">{productWithFlags.brand}</p>
-      <p className="mt-2 text-gray-600">{productWithFlags.price}</p>
+      <div className="mt-2 text-xs flex items-center gap-2">
+        <span className="text-gray-600 underline text-nowrap">
+          {productWithFlags.brand}
+        </span>
+        |
+        <span className="text-gray-600">Oferta: {productWithFlags.offer_id}</span>
+        |
+        <span className="text-gray-600">Cod. Artículo: {productWithFlags.shadow_id}</span>
+      </div>
+      <div className="text-sm text-gray-500 flex items-center gap-1">
+        {productWithFlags.seller == "PcComponentes" ? (
+          <img src={PcLogo} alt="PC Clone Logo" className="h-4" />
+        ) : (
+          <FaShop />
+        )}
+        Vendido por: <b>{productWithFlags.seller}</b>
+      </div>
     </div>
   );
 };
--- frontend/src/components/Product/ProductPriceAside.tsx
+++ frontend/src/components/Product/ProductPriceAside.tsx
@@ -7,14 +7,32 @@ export const ProductPriceAside: React.FC<ProductPriceAsideProps> = ({
   price,
   discount,
 }) => {
-  const discountedPrice = price - price * (discount / 100);
-
   return (
     <div className="mt-4">
       <h2 className="text-2xl font-semibold">Price</h2>
-      <p className="mt-2 text-gray-600">
-        <span className="line-through">${price}</span>
-        <span className="ml-2">${discountedPrice}</span>
+      <p className="mt-2 text-gray-600 flex items-center">
+        <span className="text-4xl text-red-700 font-bold mr-4">
+          {Number(price).toFixed(0)}
+          <span className="text-base text-red-700 font-bold align-super">
+            {
+              Number(price - Math.floor(price))
+                .toFixed(2)
+                .split(".")[1]
+            }
+            €
+          </span>
+        </span>
+
+        {discount != 0 ? (
+          <span className="text-gray-500 line-through mr-2 text-sm">
+            {Math.round(Number(price) / (1 - Number(discount) / 100))}€
+          </span>
+        ) : null}
+        {discount != 0 ? (
+          <span className="top-0 left-0 bg-red-700 text-white text-xs font-bold rounded px-2 py-1 z-10">
+            -{discount}%
+          </span>
+        ) : null}
       </p>
     </div>
   );
